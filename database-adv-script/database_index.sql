879a8a60575a9e74c06e53708771d95e4a2e43ef
Ayomide Johnson Salami <auomidejohnson@gmail.com>
2025-07-06 23:50:14 +0100

    included EXPLAIN ANALYZE

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -11,3 +11,11 @@ CREATE INDEX idx_booking_dates ON booking(start_date, end_date); -- Range filter
 CREATE INDEX idx_property_id ON properties(property_id); -- Join optimization
 CREATE INDEX idx_property_location ON properties(location); -- Filtering by location
 CREATE INDEX idx_property_price ON properties(price_per_night); -- Filtering by price
+
+-- EXPLAIN ANALYZE Example Query
+EXPLAIN ANALYZE SELECT * FROM users WHERE email = 'ifeoma@example.com'
+EXPLAIN ANALYZE SELECT users.first_name, users.last_name, book
+ing.property_id, booking.total_price FROM booking LEFT JOIN users ON users.us
+er_id = booking.guest_id;
+EXPLAIN ANALYSE SELECT * FROM properties WHERE price_per_night
+ < 10000;
