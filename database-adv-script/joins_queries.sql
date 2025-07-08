1698de9d8047b2d06c610c7e32bdf0c2ac065194
Ayomide Johnson Salami <auomidejohnson@gmail.com>
2025-07-08 14:13:37 +0100

    added order by

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -2,11 +2,13 @@ SELECT *
 FROM bookings as b
 INNER JOIN users as u
 ON b.user_id = u.id
+ORDER BY b.boking_id
 
 SELECT *
 FROM properties as p
 LEFT JOIN reviews as r
 ON p.property_id = r.property_id
+ORDER BY p.property_id
 
 SELECT *
 FROM users AS u
